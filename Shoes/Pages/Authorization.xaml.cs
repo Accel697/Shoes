79bf6de0e51ac50b383ca38a36451e5f2375954e
Accel <denisignatenko2006@gmail.com>
2025-11-28 15:30:31 +0700

    Добавьте файлы проекта.

---
+++ Shoes/Pages/Authorization.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shoes.Model;
+
+namespace Shoes.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnterAsGuest_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage(null));
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new shoesEntities())
+            {
+                var user = context.users.FirstOrDefault(u => u.login == tbLogin.Text.ToLower().ToString() && u.password == pbPassword.Password.ToString());
+                if (user != null)
+                {
+                    MessageBox.Show("Успешная авторизация", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.Navigate(new MainPage(user));
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    pbPassword.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Shoes/Pages/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shoes.Model;
+
+namespace Shoes.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage(users user)
+        {
+            InitializeComponent();
+            LoadProduct();
+        }
+
+        private void LoadProduct()
+        {
+            using (var context = new shoesEntities())
+            {
+                var products = context.products.ToList();
+                LviewProducts.ItemsSource = products;
+            }
+        }
+
+        private void tbSearch_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void LviewProducts_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
